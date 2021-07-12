ecbccab4452c280e2d13c5830a74b228dbe6692f
oh980225 <oh980225@gmail.com>
2021-07-12 21:13:12 +0900

    [ADD] Use MySQL DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
-insert into DIET values(1, 50, '비타민', now());
+/*insert into DIET values(1, 50, '비타민', now());
 insert into DIET values(2, 400, '샌드 위치', now());
-insert into DIET values(3, 600, '간지 치킨', now());
+insert into DIET values(3, 600, '간지 치킨', now());*/
+-- 테스트 데이터
